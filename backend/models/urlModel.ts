ab79ced14073b2b4e6a0a5a72430a028c09db155
melaku3 <melaku.aweke@astu.edu.et>
2025-03-06 13:17:49 +0300

    Refactor URL model to use generateShortId & remove unique check

--- backend/models/urlModel.ts
+++ backend/models/urlModel.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
-import { customAlphabet } from "nanoid";
-import { IUrl } from "../utils/types";
-
-const generateShortId = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 6);
+import { IUrl, generateShortId } from "../utils/types";
 
 const urlSchema = new mongoose.Schema({
     shortId: { type: String, unique: true, default: () => generateShortId() },
@@ -11,20 +8,6 @@ const urlSchema = new mongoose.Schema({
     ownerId: { type: mongoose.Types.ObjectId, ref: "User", default: null },
     expiresAt: { type: Date, default: null },
 }, { timestamps: true });
-
-// Ensure shortId is unique before saving
-urlSchema.pre<IUrl>("save", async function (next) {
-    let isUnique = false;
-    while (!isUnique) {
-        const existingUrl = await mongoose.model("URL").findOne({ shortId: this.shortId });
-        if (!existingUrl) {
-            isUnique = true;
-        } else {
-            this.shortId = generateShortId();
-        }
-    }
-    next();
-});
 
 // Expired Links: Auto-delete when `expiresAt` passes
 urlSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
